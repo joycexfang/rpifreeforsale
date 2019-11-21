982072ba86fdce9e1c832638174d7226e201d50a
iljuanss <55993599+iljuanss@users.noreply.github.com>
2019-11-21 08:28:54 -0500

    update

--- uploadform.php
+++ uploadform.php
@@ -1,21 +1,39 @@
 <?php
-  $fullname = $_POST['fullname'];
-  $freeOrSale = $_POST['freeOrSale'];
-  $title = $_POST['title'];
-  $price = $_POST['price'];
-  $myFile = $_POST['myFile'];
-  $conditions = $_POST['conditions'];
-  $categories = $_POST['categories'];
-  $description = $_POST['description'];
+
+  if (isset($_POST['fullname'])) {
+    $fullname = $_POST['fullname'];
+  }
+  if (isset($_POST['freeOrSale'])) {
+    $freeOrSale = $_POST['freeOrSale'];
+  }
+  if (isset($_POST['title'])) {
+    $title = $_POST['title'];
+  }
+  if (isset($_POST['price'])) {
+    $price = $_POST['price'];
+  }
+  if (isset($_POST['myFile'])) {
+    $myFile = $_POST['myFile'];
+  }
+  if (isset($_POST['conditions'])) {
+    $conditions = $_POST['conditions'];
+  }
+  if (isset($_POST['categories'])) {
+    $categories = $_POST['categories'];
+  }
+  if (isset($_POST['description'])) {
+    $description = $_POST['description'];
+  }
 
   //Database Connection
-  $conn = new mysqli('localhost', 'root', '', 'uploadform');
+  $conn = new mysqli('localhost', 'root', 'rootroot', 'uploadform');
   if($conn->connect_error){
     die('Connection Failed : '.$conn->connect_error);
+
   }else{
     $stmt = $conn->prepare("insert into registration(fullname, freeOrsale, title, price, myFile, conditions, categories, description)
-      values(?, ?, ?, ?, ?, ?, ?, ?,)");
-    $stmt->bind_param("sssdbsss", $fullname, $freeOrSale, $title, $price, $myFile, $conditions, $categories, $description);
+      values(?, ?, ?, ?, ?, ?, ?, ?, ?,)");
+    $stmt->bind_param("sssibsss", $fullname, $freeOrSale, $title, $price, $myFile, $conditions, $categories, $description);
     $stmt->execture();
     echo "Submitted Successfully...";
     $stmt->close();
