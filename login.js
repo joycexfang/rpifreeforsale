61afbab7cd7c11b564633e8c1b807e8b083f3576
Yuchen Luo <luoy8@rpi.edu>
2019-11-24 14:36:11 -0500

    update login system

---
+++ login.js
@@ -0,0 +1,23 @@
+var objPeople = [
+    {
+        username: "yuchen",
+        password: "123456"
+    }
+
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			alert(username + " is logged in.");
+            window.open("home.html");
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	alert("incorrect username or password")
+}
