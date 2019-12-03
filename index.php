d05424e5c0f526164cb8ee6df0060933564acd80
iljuanss <55993599+iljuanss@users.noreply.github.com>
2019-12-03 16:54:52 -0500

    update

--- index.php
+++ index.php
@@ -57,29 +57,7 @@
           </ul>
         </div>
         </div>
-<<<<<<< HEAD
-        <?php
-          // We'll need a database connection both for retrieving records and for
-          // inserting them.  Let's get it up front and use it for both processes
-          // to avoid opening the connection twice.  If we make a good connection,
-          // we'll change the $dbOk flag.
-          $dbOk = false;
 
-          /* Create a new database connection object, passing in the host, username,
-             password, and database to use. The "@" suppresses errors. */
-          @ $db = new mysqli('localhost', 'root', 'rootroot', 'rpifreeforsale');
-
-          if ($db->connect_error) {
-            echo '<div class="messages">Could not connect to the database. Error: ';
-            echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
-          } else {
-            $dbOk = true;
-          }
-          ?>
-
-=======
-      
->>>>>>> c000e3ee0da170a025e73531b976539e7aae3800
         <table id="itemListing">
         <?php
           if ($dbOk) {
